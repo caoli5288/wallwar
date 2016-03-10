9a80e115935e3f5537b497754ecb5e6227a55afd
caoli5288 <caoli5288@gmail.com>
2016-03-10 17:38:25 +0800

    Update Area.java

--- src/main/java/com/mengcraft/wallwar/level/Area.java
+++ src/main/java/com/mengcraft/wallwar/level/Area.java
@@ -1,6 +1,5 @@
 package com.mengcraft.wallwar.level;
 
-import com.google.gson.JsonObject;
 import com.mengcraft.wallwar.Main;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -29,22 +28,14 @@ public class Area implements Iterable<Location> {
     }
 
     public boolean contains(Location loc) {
-        return ((offset.getX() > 0 ? compareX(loc) > 0 : offset.getX() < 0 && compareX(loc) < 0) &&
-                (offset.getY() > 0 ? compareY(loc) > 0 : offset.getY() < 0 && compareY(loc) < 0) &&
-                (offset.getZ() > 0 ? compareZ(loc) > 0 : offset.getZ() < 0 && compareZ(loc) < 0)
+        return (check(loc.getX() - base.getX(), offset.getX()) &&
+                check(loc.getY() - base.getY(), offset.getY()) &&
+                check(loc.getZ() - base.getZ(), offset.getZ())
         );
     }
 
-    public double compareX(Location loc) {
-        return base.getX() + offset.getX() - loc.getX();
-    }
-
-    public double compareY(Location loc) {
-        return base.getY() + offset.getY() - loc.getY();
-    }
-
-    public double compareZ(Location loc) {
-        return base.getZ() + offset.getZ() - loc.getZ();
+    private boolean check(double x, double y) {
+        return (y > 0) ? (x > 0 && x < y) : (x < 0 && x > y);
     }
 
     public Location getBase() {
