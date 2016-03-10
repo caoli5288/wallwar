f1880d6c0e3ab90fba8460b020de82ae28d08384
caoli5288 <caoli5288@gmail.com>
2016-03-10 17:37:40 +0800

    Update WallBoomer.java

--- src/main/java/com/mengcraft/wallwar/level/WallBoomer.java
+++ src/main/java/com/mengcraft/wallwar/level/WallBoomer.java
@@ -33,12 +33,14 @@ public class WallBoomer extends BukkitRunnable {
     }
 
     private void process(Location next) {
-        next.getWorld().playEffect(next, Effect.EXPLOSION_LARGE, 1);
-        next.getWorld().playEffect(next, Effect.FIREWORKS_SPARK, 1);
-        if (random.nextFloat() < 0.25) {
-            next.getWorld().playSound(next, Sound.EXPLODE, 1, 1);
+        if (next.getBlock().getType() != Material.AIR) {
+            next.getWorld().playEffect(next, Effect.EXPLOSION_LARGE, 1);
+            next.getWorld().playEffect(next, Effect.FIREWORKS_SPARK, 1);
+            if (random.nextFloat() < 0.25) {
+                next.getWorld().playSound(next, Sound.EXPLODE, 1, 1);
+            }
+            next.getBlock().setType(Material.AIR);
         }
-        next.getBlock().setType(Material.AIR);
     }
 
 }
